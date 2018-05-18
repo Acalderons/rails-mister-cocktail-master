a43452cdd7558b4f369e694591c32c9cdee0f21b
Antoine Panau <a.panau93@gmail.com>
2018-05-18 14:59:12 +0200

    Cocktails project

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+require 'open-uri'
+require 'json'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredient_serialized = open(url).read
+ingredients = JSON.parse(ingredient_serialized)
+ingredients_array = ingredients["drinks"]
+ingredients_array.each do |element|
+  ingredient = element["strIngredient1"]
+  Ingredient.create(name: ingredient)
+end
